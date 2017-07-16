c2abfa0fb0e1a1ffb7155f011ef8f5fc3a5df16c
77Vincent <wentianqi77@outlook.com>
2017-07-16 23:32:51 +0800

    rename to globalStyles

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 "use strict";
 
 import React from "react";
-import { Text, View } from "react-native";
-import styles from "./src/styles.js";
+import { Text, View, TextInput, LayoutAnimation } from "react-native";
+import globalStyles from "./src/globalStyles.js";
 
 export default class App extends React.Component {
+  state = {
+  }
+
+  styles = {
+    pickContainer: {
+      flex: 4
+    },
+  }
+
+  randomColor(min, max) {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+      var result = (Math.floor(Math.random() * (max - min + 1)) + min).toString(16);
+      arr.push(result);
+    }
+    
+    return `#${arr.join("")}`;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={[globalStyles.container]}>
+        <View style={globalStyles.content}></View>
+
+        <Text style={globalStyles.logo}>Instant</Text>
       </View>
     );
   }
---
+++ src/globalStyles.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  content: {
+    flex: 23 
+  },
+  logo: {
+    flex: 1,
+    fontWeight: "bold",
+    textAlignVertical: "center",
+    textAlign: "center",
+    backgroundColor: "red"
+  },
+  input: {
+    height: 40,
+    backgroundColor: "gray"
+  }
+});
